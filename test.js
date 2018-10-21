92ae4c5c338d5a4ee36f788587849f19dfa98874
karenatalyants <karenatalyants@gmail.com>
2018-10-21 22:31:10 +0400

    moving box

--- test.js
+++ test.js
@@ -1,24 +1,43 @@
+  const canvas = document.getElementById('board');
+  const context = canvas.getContext('2d');
 
-// 5. Create a function 'forEach' that takes an array and a function and then
-// calls the given function once with each value in the given array.
+  canvas.width = 1400;
+  canvas.height = 700;
 
 
-const factorial = function (val) {
-  let a = val;
-  let fact = 1;
-  while (a>0) {
-    fact = fact * a;
-    a = a - 1;
+
+  let x = 10;
+  let y = 10;
+  let width = 50;
+  let height = 50;
+  let xDelta = 10;
+  let yDelta = 10;
+
+  const reverseDirX = function() {
+    xDelta = xDelta * (-1);
   }
-  return console.log(fact);
-};
-
-const forEach = function (array, jj) {
-  let b = 0;
-  while (b < array.length) {
-    jj(array[b]);
-    b = b + 1;
+  const reverseDirY = function() {
+    yDelta = yDelta * (-1);
   }
-};
 
-forEach([2,3,4],factorial);
+  const move = function() {
+    context.fillStyle = '#666ad1';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#001970';
+    context.fillRect(x, y, width, height);
+    if (x >= canvas.width - 50 || x <= 0) {
+      reverseDirX();
+    }
+    if (y >= canvas.height - 50 || y <= 0) {
+      reverseDirY();
+    }
+    x = x + xDelta;
+    y = y + yDelta;
+  }
+
+  const loop = function() {
+    move();
+    requestAnimationFrame(loop);
+  };
+
+  loop();
