ba1dfcc295a73b29cf02d0a9b9b64f2ab45a39e9
karenatalyants <karenatalyants@gmail.com>
2018-10-27 20:33:56 +0400

    commit

--- test.js
+++ test.js
@@ -4,38 +4,107 @@
   canvas.width = 1400;
   canvas.height = 700;
 
-  let x = 850;
-  let y = 10;
-  let width = 50;
-  let height = 50;
-  let xDelta = 10;
-  let yDelta = 15;
+  // returns a random number between 1 and the given number inclusively
+  const rand = function(num) {
+    return Math.floor(Math.random() * num) + 1;
+  };
 
-  const reverseDirX = function() {
-    xDelta = xDelta * (-1);
-  }
-  const reverseDirY = function() {
-    yDelta = yDelta * (-1);
+  // returns a random hexadecimal color code
+  const colorRandomizer = function() {
+    const hexColorArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+    let color = '#';
+    const random15 = function(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    }
+    for (let j = 1; j <= 6; j++) {
+      color = color + hexColorArr[random15(0, 15)];
+    }
+    return color;
+  };
+
+
+  let enemies = [];
+
+  // creates the boxes, of random position and random color, as many as the number given
+  const createBoxes = function(count, canvasWidth, canvasHeight) {
+
+
+
+    let i = 0;
+    for (let i = 0; i < count; i++) {
+
+      enemies[i] = {
+        x: rand(canvas.width - 50),
+        y: rand(canvas.height - 50),
+        xDelta: 10,
+        yDelta: 10,
+        width: 50,
+        height: 50,
+        color: colorRandomizer(),
+        reverseDirX: function() {
+          this.xDelta = this.xDelta * (-1);
+        },
+        reverseDirY: function() {
+          this.yDelta = this.yDelta * (-1);
+        },
+        draw: function() {
+          context.fillStyle = '#bbdefb';
+          context.fillRect(0, 0, canvas.width, canvas.height);
+          context.fillStyle = this.color;
+          context.fillRect(this.x, this.y, this.width, this.height);
+        },
+        update: function() {
+          context.fillStyle = '#bbdefb';
+          context.fillRect(0, 0, canvas.width, canvas.height);
+          context.fillStyle = this.color;
+          context.fillRect(this.x, this.y, this.width, this.height);
+
+
+          if (this.x >= canvas.width - this.width || this.x <= 0) {
+            this.reverseDirX();
+          }
+
+          if (this.y >= canvas.height - this.height || this.y <= 0) {
+            this.reverseDirY();
+          }
+
+          this.x = this.x + this.xDelta;
+          this.y = this.y + this.yDelta;
+
+
+        }
+      }
+    }
+
+    return enemies;
+  };
+
+
+  createBoxes(25, canvas.width, canvas.height);
+
+  console.log(enemies);
+
+
+  const draw = function() {
+    for (let d = 0; d < enemies.length; d++) {
+      const obj = enemies[d];
+      obj.draw();
+    }
   }
 
-  const move = function() {
-    context.fillStyle = '#666ad1';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = '#001970';
-    context.fillRect(x, y, width, height);
-    if (x >= canvas.width - 50 || x <= 0) {
-      reverseDirX();
+  const update = function() {
+    for (let d = 0; d < enemies.length; d++) {
+      const obj = enemies[d];
+      obj.update();
     }
-    if (y >= canvas.height - 50 || y <= 0) {
-      reverseDirY();
-    }
-    x = x + xDelta;
-    y = y + yDelta;
   }
 
   const loop = function() {
-    move();
+    draw();
+    update();
     requestAnimationFrame(loop);
   };
 
-  loop();
+  loop()
